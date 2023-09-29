6fe04e0bec61f0cc4c10c0d90a735cb3b0bd749f
Rafael Füllenbach <rafael.fullenbach12@gmail.com>
2023-09-29 15:50:14 -0300

    style:refactoring and removing spaces

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SignIn from './pages/SignIn'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SignIn from "./pages/SignIn";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SignIn />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
