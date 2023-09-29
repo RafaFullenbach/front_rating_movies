1e584850ddd20b30157c26c794036835cf7a0f83
Rafael Füllenbach <rafael.fullenbach12@gmail.com>
2023-09-29 19:38:44 -0300

    feat: creating signIn page

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,3 +1,26 @@
+import { Container, Background, Form } from "./styles";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { FiLock, FiMail } from "react-icons/fi";
+
 export default function SignIn() {
-  return;
+  return (
+    <Container>
+      <Form>
+        <h1>RatingMovies</h1>
+        <p>Aplicação para acompanhar tudo que assistir.</p>
+
+        <h2>Faça seu login</h2>
+
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
+
+        <Input placeholder="Senha" type="password" icon={FiLock} />
+
+        <Button title="Entrar" />
+
+        <a href="#">Criar conta</a>
+      </Form>
+      <Background />
+    </Container>
+  );
 }
