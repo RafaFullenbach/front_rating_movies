4f43121aca248fe9b4c375b04e609e4aab81f050
Rafael Füllenbach <rafael.fullenbach12@gmail.com>
2023-09-29 16:40:54 -0300

    feat: implementing global themes and stylizations

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,3 +1,3 @@
 export default function SignIn() {
-  return <h1>Hello Wolrd!</h1>;
+  return;
 }
---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "hsla(255, 5%, 15%, 1)",
+    BACKGROUND_800: "rgba(255, 133, 155, 0.05)",
+    BACKGROUND_700: "hsla(256, 10%, 20%, 1)",
+    GRAY_300: "hsla(26, 4%, 59%, 1))",
+    GRAY_200: "hsla(270, 5%, 58%, 1)",
+    GRAY_100: "hsla(273, 10%, 79%, 1)",
+    WHITE: "hsla(26, 37%, 93%, 1)",
+    PINK: "hsla(349, 100%, 76%, 1)",
+  },
+};
+
+
