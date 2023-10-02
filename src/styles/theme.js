dd699210b7af8c3409d7a38436dc7fd124d0be7f
Rafael Füllenbach <rafael.fullenbach12@gmail.com>
2023-10-02 17:14:33 -0300

    feat: add new color

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,6 +3,7 @@ export default {
     BACKGROUND_900: "hsla(255, 5%, 15%, 1)",
     BACKGROUND_800: "rgba(255, 133, 155, 0.05)",
     BACKGROUND_700: "hsla(256, 10%, 20%, 1)",
+    BACKGROUND_600: "hsla(255, 9%, 25%, 1)",
     GRAY_300: "hsla(26, 4%, 59%, 1))",
     GRAY_200: "hsla(270, 5%, 58%, 1)",
     GRAY_100: "hsla(273, 10%, 79%, 1)",
