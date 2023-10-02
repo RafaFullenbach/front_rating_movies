6cc3918647f8f4572d8b2864d8e6a0d482511578
Rafael Füllenbach <rafael.fullenbach12@gmail.com>
2023-10-02 15:13:50 -0300

    feat: creating page signUp

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,32 @@
+import { Container, Background, Form } from "./styles";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { FiLock, FiMail, FiUser } from "react-icons/fi";
+import { MdArrowBack } from "react-icons/md";
+
+export default function SignIn() {
+  return (
+    <Container>
+      <Form>
+        <h1>RatingMovies</h1>
+        <p>Aplicação para acompanhar tudo que assistir.</p>
+
+        <h2>Crie sua conta</h2>
+
+        <Input placeholder="Nome" type="text" icon={FiUser} />
+
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
+
+        <Input placeholder="Senha" type="password" icon={FiLock} />
+
+        <Button title="Entrar" />
+
+        <a href="#">
+          <MdArrowBack size={20} />
+          Voltar para o login
+        </a>
+      </Form>
+      <Background />
+    </Container>
+  );
+}
---
+++ src/pages/SignUp/styles.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+import backgroundImg from "../../assets/movie_theater.png";
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Form = styled.form`
+  padding: 0 16.3rem 0 13.4rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  > h1 {
+    color: ${({ theme }) => theme.COLORS.PINK};
+    font-size: 4.8rem;
+    font-weight: 700;
+  }
+
+  > p {
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+    font-size: 1.4rem;
+  }
+
+  > h2 {
+    font-size: 2.4rem;
+    font-weight: 500;
+
+    margin: 4.8rem 0;
+  }
+
+  > a {
+    color: ${({ theme }) => theme.COLORS.PINK};
+    text-align: center;
+
+    margin-top: 4.2rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.2rem;
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${backgroundImg}) no-repeat center center;
+  background-size: cover;
+`;
