4c109a2db242daccf38110b44f39a79b92668ab8
Rafael Füllenbach <rafael.fullenbach12@gmail.com>
2023-10-04 19:11:43 -0300

    feat: add route new

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import { Home } from "../pages/Home";
 import { Profile } from "../pages/Profile";
+import { New } from "../pages/New";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/new" element={<New />} />
     </Routes>
   );
 }
